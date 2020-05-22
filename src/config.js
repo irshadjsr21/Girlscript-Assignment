fa92298ae4984f3f03a3c5b37c1640c64c862f5d
Irshad Ansari <irshadjsr21@gmail.com>
2020-05-23 03:50:31 +0530

    Add google sheets API

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  accessToken: process.env.REACT_APP_SHEETS_ACCESS_TOKEN,
+  sheetId: '1MGDHtYGEsUwbU23x5WumzBWInsJRRil34t27Q9xn1e0',
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
+import config from '../config';
 
 export default function Home() {
+  const getSheetValues = async () => {
+    const request = await fetch(
+      `https://sheets.googleapis.com/v4/spreadsheets/${config.sheetId}/values/A:D`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${config.accessToken}`,
+        },
+      },
+    );
+    const data = await request.json();
+    console.log(data);
+  };
+
+  React.useEffect(() => {
+    getSheetValues();
+  });
+
   return (
     <Jumbotron>
       <h1>Welcome to Girlscript website!</h1>
