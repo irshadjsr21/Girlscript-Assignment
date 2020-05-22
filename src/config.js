f7258506854b90bf3f3cd9b69c9179fe55d71e3b
Irshad Ansari <irshadjsr21@gmail.com>
2020-05-23 03:26:49 +0530

    Added google sheets API

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  apiKey: process.env.REACT_APP_SHEETS_API_KEY,
+  discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  spreadsheetId: '1MGDHtYGEsUwbU23x5WumzBWInsJRRil34t27Q9xn1e0',
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
+import config from '../config';
 
 export default function Home() {
+  const initClient = () => {
+    // 2. Initialize the JavaScript client library.
+    window.gapi.client
+      .init({
+        apiKey: config.apiKey,
+        discoveryDocs: config.discoveryDocs,
+      })
+      .then(() => {
+        // 3. Initialize and make the API request.
+        window.gapi.client.load('sheets', 'v4', () => {
+          window.gapi.client.sheets.spreadsheets.values
+            .get({
+              spreadsheetId: config.spreadsheetId,
+              range: 'Sheet1!A4:T',
+            })
+            .then(
+              (response) => {
+                const data = response.result.values;
+                console.log(data);
+              },
+              (response) => {
+                console.log(response.result.error);
+              },
+            );
+        });
+      });
+  };
+
+  React.useEffect(() => {
+    console.log(config);
+    window.gapi.load('client', initClient);
+  }, []);
+
   return (
     <Jumbotron>
       <h1>Welcome to Girlscript website!</h1>
